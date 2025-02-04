10881e590cb1cf5c06410995071992da1c1358a0
DCodeMaster <111651665+david-software-2006@users.noreply.github.com>
2025-02-04 12:31:10 -0500

    CRUD

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,13 +1,25 @@
+﻿using ProductsApp.Models;
 using Microsoft.EntityFrameworkCore;
-using CRUD.Models;
 
-namespace CRUD.Data;
-
-public class AppDbContext : DbContext
+namespace ProductsApp.Data
 {
-    public DbSet<Product> Products { get; set; }
-
-    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Products> Products { get; set; }
+
+        public DbSet<ProductDetail> ProductDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ProductDetail>()
+                .HasOne(pd => pd.Product)
+                .WithOne(p => p.ProductDetail)
+                .HasForeignKey<ProductDetail>(pd => pd.ProductId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,36 +1,30 @@
-using CRUD.Repositories;
-using CRUD.Services;
-using CRUD.Data;
+using ProductsApp.Data;
+using ProductsApp.Repositories;
+using ProductsApp.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase("AppProductos"));
-builder.Services.AddScoped<IProductoRepository, ProductoRepository>();
-builder.Services.AddScoped<IProductoService, ProductoService>();
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<IProductDetailRepository, ProductDetailRepository>();
+builder.Services.AddScoped<IProductService, ProductService>();
+
+
+builder.Services.AddControllers();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+app.MapControllers();
 
 app.Run();
