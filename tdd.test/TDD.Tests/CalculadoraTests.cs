768c002dad97a6a02f8492a6f28efeb455be0353
DCodeMaster <111651665+david-software-2006@users.noreply.github.com>
2025-02-17 21:24:16 -0500

    Mi primera prueba TDD

---
+++ tdd.test/TDD.Tests/CalculadoraTests.cs
@@ -0,0 +1,19 @@
+﻿using Xunit;
+
+namespace TDD.Tests;
+
+public class CalculadoraTests
+{
+    [Fact]
+    public void Sumar_DeberiaRetornarLaSumaDeDosNumeros()
+    {
+        // Arrange
+        var calculadora = new Calculadora();
+
+        // Act
+        int resultado = calculadora.Sumar(2, 3);
+
+        // Assert
+        Assert.Equal(5, resultado);
+    }
+}
